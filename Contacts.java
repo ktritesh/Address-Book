d691691554df4aeff3c27552634726f075363997
ktritsh7 <ktriesh7@gmail.com>
2021-07-10 11:38:31 +0530

    Ability to view person by city or state

--- Contacts.java
+++ Contacts.java
@@ -11,52 +11,33 @@
 package com.bridgelabz.addressbook;
 
 public class Contacts {
+    private String fName;
+    private String lName;
+    private String address;
+    private String city;
+    private String state;
+    private Long zip;
+    private Long phone;
+    private String email;
 
-    //Instance variable
-    String firstName;
-    String lastName;
-    String address;
-    String city;
-    String state;
-    long zipCode;
-    long phoneNumber;
-    String email;
-
-    public Contacts() {
-    }
-
-    public Contacts(String firstName, String lastName, String address, String city, String state, long zipCode, long phoneNumber, String email) {
-        this.firstName = firstName;
-        this.lastName = lastName;
+    public Contacts(String fName, String lName, String address, String city, String state, Long zip, Long phone,
+                    String email) {
+        this.fName = fName;
+        this.lName = lName;
         this.address = address;
         this.city = city;
         this.state = state;
-        this.zipCode = zipCode;
-        this.phoneNumber = phoneNumber;
+        this.zip = zip;
+        this.phone = phone;
         this.email = email;
     }
 
-    @Override
-    public String toString() {
-        return "Contacts{" +
-                "firstName='" + firstName + '\'' +
-                ", lastName='" + lastName + '\'' +
-                ", address='" + address + '\'' +
-                ", city='" + city + '\'' +
-                ", state='" + state + '\'' +
-                ", zipCode=" + zipCode +
-                ", phoneNumber=" + phoneNumber +
-                ", email='" + email + '\'' +
-                '}';
+    public String getfName() {
+        return fName;
     }
 
-    // Creating getter and setter method
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
+    public void setfName(String fName) {
+        this.fName = fName;
     }
 
     public String getCity() {
@@ -74,4 +55,64 @@ public class Contacts {
     public void setState(String state) {
         this.state = state;
     }
+
+    public String getlName() {
+        return lName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public Long getZip() {
+        return zip;
+    }
+
+    public Long getPhone() {
+        return phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (!(obj instanceof Contacts))
+            return false;
+
+        Contacts other = (Contacts) obj;
+        if (!(fName.equalsIgnoreCase(other.getfName())))
+            return false;
+        if (!(lName.equalsIgnoreCase(other.getlName())))
+            return false;
+        if (!(address.equalsIgnoreCase(other.getAddress())))
+            return false;
+        if (!(city.equalsIgnoreCase(other.getCity())))
+            return false;
+        if (!state.equalsIgnoreCase(other.getState()))
+            return false;
+        if (!zip.equals(other.getZip()))
+            return false;
+        if (!(phone.equals(other.getPhone())))
+            return false;
+        return email.equals(other.getEmail());
+    }
+
+    @Override
+    public String toString() {
+        return
+                "\nFirst_Name  : " + fName +
+                        "\nLast_Name   : " + lName +
+                        "\nAddress     : " + address +
+                        "\nCity        : " + city +
+                        "\nState       : " + state +
+                        "\nZIP Code    : " + zip +
+                        "\nPhone_Number: " + phone +
+                        "\nEmail_ID    : " + email +
+                        "\n-----------------------------------";
+    }
+
 }
